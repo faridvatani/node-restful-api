dd395c50c0943969cb885478635033c00f309fcf
Farid Vatani <faridvatani@gmail.com>
2024-08-21 23:43:30 +0330

    chore: Add helper functions for random generation and password authentication

---
+++ src/helpers/index.ts
@@ -0,0 +1,14 @@
+import crypto from "crypto";
+
+const secret = process.env.SECRET || "secret";
+
+export const random = (length: number = 128): string => {
+  return crypto.randomBytes(length).toString("base64");
+};
+
+export const authentication = (password: string, salt: string): string => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(secret)
+    .digest("hex");
+};

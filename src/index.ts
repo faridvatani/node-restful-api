80aa51ebf87ed616fad9117a47bf831bcaddc4ab
Farid Vatani <faridvatani@gmail.com>
2024-08-21 14:47:55 +0330

    chore: Add MongoDB connection and configuration

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 // Load environment variables from .env file in development
 if (process.env.NODE_ENV !== "production") {
@@ -35,4 +36,26 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
+});
+
+// Connect to MongoDB
+const dbURI = process.env.MONGO_URL;
+if (!dbURI) {
+  throw new Error("MONGO_URL is not defined in the environment variables");
+}
+
+mongoose.Promise = Promise;
+mongoose.connect(dbURI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+  process.exit(-1);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected from DB");
 });
